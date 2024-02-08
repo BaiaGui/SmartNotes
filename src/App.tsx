8dde971642fb8aecab59b6928de06a295085fc1d
Guilherme Baia <guilhermebaiav@gmail.com>
2024-02-08 19:14:04 -0300

    adds search feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/LogoNLW.svg'
 import { NewNoteCard } from './components/NewNoteCard'
 import { NoteCard } from './components/NoteCard'
@@ -12,8 +12,12 @@ export function App() {
       content: string
   }
 
+  const [shouldShowAddNoteCard, setShouldShowAddNoteCard] = useState(true)
+  const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<Note[]>(()=>{
-    const storagedNotes = localStorage.getItem('notes');
+
+
+  const storagedNotes = localStorage.getItem('notes');
 
     if(storagedNotes)
       return JSON.parse(storagedNotes)
@@ -33,19 +37,33 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray) )
   }
 
+  function handleSearch(event: ChangeEvent<HTMLInputElement>){
+    const searchValue = event.target.value
+    setSearch(searchValue);
+    if(searchValue !== '')
+      setShouldShowAddNoteCard(false)
+    else
+      setShouldShowAddNoteCard(true)
+  }
+  
+  
+
+
+  
+  const filteredNotes = search !== ''?notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())):notes
 
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="NLW Expert" />
       <form className='w-full'>
-        <input type="text" placeholder='Busque em suas notas...' 
+        <input type="text" onChange={handleSearch} value={search} placeholder='Busque em suas notas...' 
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500' />
       </form>
       <div className='h-px bg-slate-700'/>
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <NewNoteCard onNoteCreated={onNoteCreated}/>
-        {notes.map(note => {return <NoteCard note={note} key={note.id} /> })}
+        {shouldShowAddNoteCard&&<NewNoteCard onNoteCreated={onNoteCreated}/>}
+        {filteredNotes.length !== 0 ?filteredNotes.map(note => {return <NoteCard note={note} key={note.id} /> }):<p className='text-slate-400'>Nota não encontrada...</p>}
 
 
       </div>    
